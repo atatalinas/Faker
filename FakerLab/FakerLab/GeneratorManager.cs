7d8c3d387e83d19689878c826b88bd788a011a7f
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-18 18:38:41 +0300

    added generators manager

---
+++ FakerLab/FakerLab/GeneratorManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using PluginInterface;
+
+
+namespace Faker
+{
+    public class GeneratorManager : IGenerator
+    {
+       
+        private List<IGenerator> generators;
+
+        private readonly string PLUGINS_PATH = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+
+        public GeneratorManager()
+        {
+            var loader = new PluginsLoader<IGenerator>(PLUGINS_PATH);
+
+            generators = loader.Load();
+        }
+        public bool IsGeneratable(Type type)
+        {
+            foreach (var generator in generators)
+            {
+                if (generator.IsGeneratable(type))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public object Generate(Type type)
+        {
+            foreach (var generator in generators)
+            {
+                if (generator.IsGeneratable(type))
+                {
+                    return generator.Generate(type);
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- FakerLab/PluginInterface/IGenerator.cs
+++ FakerLab/PluginInterface/IGenerator.cs
@@ -1,6 +1,10 @@
-﻿namespace PluginInterface
+﻿using System;
+
+namespace PluginInterface
 {
     public interface IGenerator
     {
+        object Generate(Type type);
+        bool IsGeneratable(Type type);
     }
 }
