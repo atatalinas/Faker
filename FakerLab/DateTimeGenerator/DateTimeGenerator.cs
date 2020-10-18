a50f09b18efb937137ce13bb754eecbfd59e9adf
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-18 18:23:04 +0300

    added date-time generator implementation

--- FakerLab/DateTimeGenerator/DateTimeGenerator.cs
+++ FakerLab/DateTimeGenerator/DateTimeGenerator.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PluginInterface;
 
 namespace DateTimeGenerator
 {
-    public class DateTimeGenerator
+    public class DateTimeGenerator : IGenerator
     {
+        private readonly Random random;
+
+        public DateTimeGenerator()
+        {
+            random = new Random();
+        }
+
+        public bool IsGeneratable(Type type)
+        {
+            return type == typeof(DateTime);
+        }
+
+        public object Generate(Type type)
+        {
+            return new DateTime(random.Next(0, DateTime.Now.Year), random.Next(1, 12), random.Next(1, 30),
+                random.Next(0, 24), random.Next(0, 60), random.Next(0, 60));
+        }
     }
 }
