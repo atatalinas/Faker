f3151e1efabac358b3121e781549ab4ce88ad3ff
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-18 16:16:19 +0300

    first commit

---
+++ FakerLab/FakerLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FakerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
